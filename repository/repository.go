537cd21b16fba0e5c9d97fb167185480dd5256ca
dragfire <asem.devajit@gmail.com>
2018-02-17 14:23:46 +0530

    Add comments for exported methods

--- repository/repository.go
+++ repository/repository.go
@@ -5,10 +5,12 @@ import (
 	"gopkg.in/mgo.v2"
 )
 
+// Repository maintains a mgo.Collection
 type Repository struct {
 	*mgo.Collection
 }
 
+// NewRepository creates a new Repository
 func NewRepository(mongo *config.Mongo, collectionName string) *Repository {
 	return &Repository{mongo.C(collectionName)}
 }
--- repository/user.go
+++ repository/user.go
@@ -2,18 +2,29 @@ package repository
 
 import (
 	"bitbucket.org/hayum/hayum-service/models"
+	"gopkg.in/mgo.v2/bson"
 )
 
+// UserRepository embeds a Repository
 type UserRepository struct {
 	*Repository
 }
 
+// NewUserRepository creates a new UserRepository
 func NewUserRepository(r *Repository) *UserRepository {
 	return &UserRepository{r}
 }
 
-func (repository *UserRepository) GetUserById(id string) (*models.User, error) {
+// GetUserByID gets user by Id
+func (r *UserRepository) GetUserByID(id string) (*models.User, error) {
 	var user *models.User
-	err := repository.FindId(id).One(&user)
+	err := r.FindId(id).One(&user)
+	return user, err
+}
+
+// GetUserByEmail gets user by email
+func (r *UserRepository) GetUserByEmail(email string) (*models.User, error) {
+	var user *models.User
+	err := r.Find(bson.M{"email": email}).One(&user)
 	return user, err
 }
