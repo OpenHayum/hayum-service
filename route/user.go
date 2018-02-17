9d3ff8e2976b890b2c2a814cd2f22cfe9df1077d
dragfire <asem.devajit@gmail.com>
2018-02-17 12:44:48 +0530

    Simplify route construction

--- route/user.go
+++ route/user.go
@@ -1,7 +1,6 @@
 package route
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -26,7 +25,9 @@ type userRoute struct {
 func initUserRoute(r *Router, basePath string) {
 	service := service.NewUserService(r.Mongo)
 	route := &userRoute{Router: r, service: service, basePath: basePath}
-	route.getUser()
+
+	route.POST(util.ConstructEndpoint(basePath, "/user"), route.createUser)
+	route.GET(util.ConstructEndpoint(basePath, "/user/:id"), route.getUser)
 }
 
 func (u *userRoute) createUser(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
@@ -36,13 +37,11 @@ func (u *userRoute) createUser(w http.ResponseWriter, r *http.Request, ps httpro
 	schemaDecoder.Decode(user, r.PostForm)
 }
 
-func (u *userRoute) getUser() {
-	u.GET(util.ConstructEndpoint(u.basePath, "/user/:id"), func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-		user, err := u.service.GetUserById(ps.ByName("id"))
-		if err != nil {
-			log.Println("Cannot get user id")
-		}
-		fmt.Fprintf(w, "User Id: %s!\n", ps.ByName("id"))
-		json.NewEncoder(w).Encode(user)
-	})
+func (u *userRoute) getUser(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	user, err := u.service.GetUserById(ps.ByName("id"))
+	if err != nil {
+		log.Println("Cannot get user id")
+	}
+	fmt.Fprintf(w, "User Id: %s!\n", ps.ByName("id"))
+	u.Send(w, user)
 }
