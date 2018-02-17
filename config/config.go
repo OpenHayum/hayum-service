539806af55c2983c7318d7316d335fddd134be49
dragfire <asem.devajit@gmail.com>
2018-02-17 10:08:21 +0530

    Add config loader

--- config/config.go
+++ config/config.go
@@ -1,6 +1,23 @@
 package config
 
-const (
-	Port  = ":8080"
-	DbUri = "localhost:27017"
+import (
+	"fmt"
+
+	"github.com/spf13/viper"
 )
+
+func LoadConfig(configPaths ...string) (*viper.Viper, error) {
+	v := viper.New()
+	v.SetConfigName("config")
+	v.SetConfigType("yaml")
+	v.AutomaticEnv()
+	for _, path := range configPaths {
+		v.AddConfigPath(path)
+	}
+
+	if err := v.ReadInConfig(); err != nil {
+		return nil, fmt.Errorf("Failed to read the configuration file: %s", err)
+	}
+
+	return v, nil
+}
