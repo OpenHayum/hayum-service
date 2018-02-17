c5fd578588c139dca5ac3891d301790c2d1c5407
dragfire <asem.devajit@gmail.com>
2018-02-17 10:56:36 +0530

    Add mongo instance to route

--- route/router.go
+++ route/router.go
@@ -1,6 +1,7 @@
 package route
 
 import (
+	"bitbucket.org/hayum/hayum-service/config"
 	"bitbucket.org/hayum/hayum-service/util"
 	"github.com/julienschmidt/httprouter"
 )
@@ -9,6 +10,7 @@ const apiVersion1 = "/api/v1/"
 
 type Router struct {
 	*httprouter.Router
+	*config.Mongo
 }
 
 func (r *Router) Init() {
