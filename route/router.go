2b413c00825b56bb228465f37d5276a283bb83a3
dragfire <asem.devajit@gmail.com>
2018-02-17 10:09:18 +0530

    Add IteRoute

--- route/router.go
+++ route/router.go
@@ -1,11 +1,17 @@
 package route
 
-import "github.com/julienschmidt/httprouter"
+import (
+	"bitbucket.org/hayum/hayum-service/util"
+	"github.com/julienschmidt/httprouter"
+)
+
+const apiVersion1 = "/api/v1/"
 
 type Router struct {
 	*httprouter.Router
 }
 
 func (r *Router) Init() {
-	r.UserRoute("/user")
+	r.UserRoute(util.ConstructEndpoint(apiVersion1, "/user/"))
+	r.ItemRoute(util.ConstructEndpoint(apiVersion1, "/item/"))
 }
