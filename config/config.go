38f55531d43fc76db5da1ebde244b6cfc41fc152
dragfire <asem.devajit@gmail.com>
2018-02-22 09:37:07 +0530

    Add collection names

--- config/config.go
+++ config/config.go
@@ -6,6 +6,24 @@ import (
 	"github.com/spf13/viper"
 )
 
+const (
+	// CollectionUser collection name for users
+	CollectionUser = "users"
+
+	// CollectionArtist collection name for artists
+	CollectionArtist = "artists"
+
+	// CollectionAudio collection name for audios
+	CollectionAudio = "audios"
+
+	// CollectionMessage collection name for messages
+	CollectionMessage = "messages"
+
+	// CollectionS3Document collection name for S3 Documents
+	CollectionS3Document = "s3Documents"
+)
+
+// LoadConfig loads any yaml config file
 func LoadConfig(configPaths ...string) (*viper.Viper, error) {
 	v := viper.New()
 	v.SetConfigName("config")
