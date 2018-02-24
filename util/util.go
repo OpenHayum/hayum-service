a1fc50aa082423e58472089ced92398c2cf0e31f
dragfire <asem.devajit@gmail.com>
2018-02-24 23:31:08 +0530

    Add GenerateOTP

--- util/util.go
+++ util/util.go
@@ -1,10 +1,16 @@
 package util
 
 import (
+	"math/rand"
 	"path"
 )
 
-// ConstructRoute constructs API endpoint
+// ConstructEndpoint constructs API endpoint
 func ConstructEndpoint(basePath string, pathName string) string {
 	return path.Join(basePath, pathName)
+}
+
+// GenerateOTP generates OTP which will be used for SMS OTP verification
+func GenerateOTP() int {
+	return rand.Intn(10000)
 }
