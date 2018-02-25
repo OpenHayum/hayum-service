95091b8d7280a096151b7095e26445fb70cd15f8
dragfire <asem.devajit@gmail.com>
2018-02-25 22:16:44 +0530

    Add BaseRepository interface with some common repository methods

--- repository/repository.go
+++ repository/repository.go
@@ -5,12 +5,31 @@ import (
 	"gopkg.in/mgo.v2"
 )
 
+// BaseRepository defines all default repository methods
+type BaseRepository interface {
+	Save(model interface{}) error
+	GetByID(id string) (interface{}, error)
+}
+
 // Repository maintains a mgo.Collection
 type Repository struct {
-	*mgo.Collection
+	collection *mgo.Collection
 }
 
 // NewRepository creates a new Repository
 func NewRepository(mongo *config.Mongo, collectionName string) *Repository {
 	return &Repository{mongo.C(collectionName)}
+}
+
+// Save implements BaseRepository Save
+func (r *Repository) Save(model interface{}) error {
+	err := r.collection.Insert(model)
+	return err
+}
+
+// GetByID implements BaseRepository GetByID
+func (r *Repository) GetByID(id string) (interface{}, error) {
+	var model interface{}
+	err := r.collection.FindId(id).One(model)
+	return &model, err
 }
