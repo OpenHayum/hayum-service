f9e4851a1ac5fb28af5be2d6f0f3ffae5754e0c5
dragfire <asem.devajit@gmail.com>
2018-03-12 09:40:18 +0530

    Add core package

---
+++ core/royalty.go
@@ -0,0 +1,15 @@
+package core
+
+const (
+	// MinArtistPopularityRate is the min popularity of the artist
+	MinArtistPopularityRate = 0
+
+	// MaxArtistPopularityRate is the max popularity of the artist
+	MaxArtistPopularityRate = 1
+
+	// MinAllowedAmount is the min amount allowed for disbursal
+	MinAllowedAmount = 0
+
+	// MaxAllowedAmount is the max amount allowed for disbursal
+	MaxAllowedAmount = 50
+)
