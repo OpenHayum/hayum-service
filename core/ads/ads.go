c4d5af941b2b17ede8f8d8e1ee00e83725f4a696
dragfire <asem.devajit@gmail.com>
2018-03-16 09:07:27 +0530

    Add ads types

--- core/ads/ads.go
+++ core/ads/ads.go
@@ -1,1 +1,17 @@
 package ads
+
+const (
+	// TypeAudio is Audio ad
+	TypeAudio = "AD_AUDIO"
+
+	// TypeImage is Image ad
+	TypeImage = "AD_IMAGE"
+
+	// TypeVideo is Video ad
+	TypeVideo = "AD_VIDEO"
+)
+
+// Ads stores an ads info
+type Ads struct {
+	Type string
+}
