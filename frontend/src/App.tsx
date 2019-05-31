ea1ac74db24f5e0a1f834698325eb349f1ae39cc
dragfire <asem.devajit@gmail.com>
2019-05-31 12:43:42 -0400

    Add simple-line-icons just for AudioPlayer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,81 +1,84 @@
 import React from 'react';
 import {Breadcrumb, Icon, Layout, Menu} from 'antd';
 import './App.less';
+import AudioPlayer from "./common/AudioPlayer";
 
 const {Header, Content, Footer, Sider} = Layout;
 const {SubMenu} = Menu;
 
 class App extends React.Component<any, any> {
-    state = {
-        collapsed: false,
-    };
+  state = {
+    collapsed: false,
+  };
 
-    onCollapse = (collapsed: any) => {
-        console.log(collapsed);
-        this.setState({collapsed});
-    };
+  onCollapse = (collapsed: any) => {
+    console.log(collapsed);
+    this.setState({collapsed});
+  };
 
-    render() {
-        return (
-            <Layout style={{minHeight: '100vh'}}>
-                <Sider collapsible collapsed={this.state.collapsed} onCollapse={this.onCollapse}>
-                    <div className="logo"/>
-                    <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-                        <Menu.Item key="1">
-                            <Icon type="pie-chart"/>
-                            <span>Home</span>
-                        </Menu.Item>
-                        <Menu.Item key="2">
-                            <Icon type="desktop"/>
-                            <span>Music</span>
-                        </Menu.Item>
-                        <SubMenu
-                            key="sub1"
-                            title={
-                                <span>
+  render() {
+    return (
+        <Layout style={{minHeight: '100vh'}}>
+          <Sider collapsible collapsed={this.state.collapsed} onCollapse={this.onCollapse}>
+            <div className="logo"/>
+            <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+              <Menu.Item key="1">
+                <Icon type="pie-chart"/>
+                <span>Home</span>
+              </Menu.Item>
+              <Menu.Item key="2">
+                <Icon type="desktop"/>
+                <span>Music</span>
+              </Menu.Item>
+              <SubMenu
+                  key="sub1"
+                  title={
+                    <span>
                                   <Icon type="user"/>
                                   <span>Artist</span>
                                 </span>
-                            }
-                        >
-                            <Menu.Item key="3">Tommy</Menu.Item>
-                            <Menu.Item key="4">Joshua</Menu.Item>
-                            <Menu.Item key="5">Cheever</Menu.Item>
-                        </SubMenu>
-                        <SubMenu
-                            key="sub2"
-                            title={
-                                <span>
+                  }
+              >
+                <Menu.Item key="3">Tommy</Menu.Item>
+                <Menu.Item key="4">Joshua</Menu.Item>
+                <Menu.Item key="5">Cheever</Menu.Item>
+              </SubMenu>
+              <SubMenu
+                  key="sub2"
+                  title={
+                    <span>
                                   <Icon type="team"/>
                                   <span>Followers</span>
                                 </span>
-                            }
-                        >
-                            <Menu.Item key="6">Team 1</Menu.Item>
-                            <Menu.Item key="8">Team 2</Menu.Item>
-                        </SubMenu>
-                        <Menu.Item key="9">
-                            <Icon type="file"/>
-                            <span>Upload Music</span>
-                        </Menu.Item>
-                    </Menu>
-                </Sider>
-                <Layout>
-                    <Header style={{background: '#fff', padding: 0}}/>
-                    <Content style={{margin: '0 16px'}}>
-                        <Breadcrumb style={{margin: '16px 0'}}>
-                            <Breadcrumb.Item>Music</Breadcrumb.Item>
-                            <Breadcrumb.Item>Good Things Fall Apart</Breadcrumb.Item>
-                        </Breadcrumb>
-                        <div style={{padding: 24, background: '#fff', minHeight: 360}}>
-                            Artist: Jon Bellion & Illenium
-                        </div>
-                    </Content>
-                    <Footer style={{textAlign: 'center'}}>©2019 Hayum</Footer>
-                </Layout>
-            </Layout>
-        );
-    }
+                  }
+              >
+                <Menu.Item key="6">Team 1</Menu.Item>
+                <Menu.Item key="8">Team 2</Menu.Item>
+              </SubMenu>
+              <Menu.Item key="9">
+                <Icon type="file"/>
+                <span>Upload Music</span>
+              </Menu.Item>
+            </Menu>
+          </Sider>
+          <Layout>
+            <Header style={{background: '#fff', padding: 0}}/>
+            <Content style={{margin: '0 16px'}}>
+              <Breadcrumb style={{margin: '16px 0'}}>
+                <Breadcrumb.Item>Music</Breadcrumb.Item>
+                <Breadcrumb.Item>Good Things Fall Apart</Breadcrumb.Item>
+              </Breadcrumb>
+              <div style={{padding: 24, background: '#fff', minHeight: 360}}>
+                Artist: Jon Bellion & Illenium
+              </div>
+            </Content>
+            <Footer style={{textAlign: 'center', padding: 0, height: 100}}>
+              <AudioPlayer/>
+            </Footer>
+          </Layout>
+        </Layout>
+    );
+  }
 }
 
 export default App;
---
+++ frontend/src/common/AudioPlayer/index.tsx
@@ -0,0 +1,251 @@
+import React, {Component} from "react";
+import classnames from "classnames";
+import * as PropTypes from "prop-types";
+
+import "./audioPlayer.less";
+
+const controlNames = {
+  IS_PLAYING: "isPlaying",
+  IS_PAUSED: 'isPaused',
+  IS_RESET: 'isReset',
+};
+
+class AudioPlayer extends Component<any, any> {
+  static propTypes = {
+    src: PropTypes.string
+  };
+
+  static defaultProps = {
+    src: "http://www.panacherock.com/downloads/mp3/01_Afterlife.mp3"
+  };
+
+  timeline: any;
+  timelineWidth: any;
+  playhead: any;
+  mouseOnPlayhead: any;
+  duration: any;
+  player: any;
+  handleEnded: any;
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isPlaying: false,
+      playheadPosition: 0,
+      activeTimelineWidth: 0,
+      currentDuration: "00:00",
+      totalDuration: this.formatTime(null)
+    };
+
+    this.timeline = null;
+    this.timelineWidth = null;
+    this.playhead = null;
+    this.mouseOnPlayhead = false;
+    this.duration = null;
+    this.player = null;
+  }
+
+  componentDidMount() {
+    window.addEventListener("mouseup", this.mouseUp, false);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("mouseup", this.mouseUp, false);
+    this.player.removeEventListener('canplaythrough', this.handleCanPlayThrough, false);
+    this.player.removeEventListener('timeupdate', this.handleTimeUpdate, false);
+    this.player.removeEventListener('ended', this.handleEnded, false);
+  }
+
+  clickPercent = (event) => {
+    const {left, width} = this.getPosition(this.timeline);
+    return (event.clientX - left) / width;
+  };
+
+  formatTime = seconds => {
+    if (!!seconds === false) return "00:00";
+
+    let minutes: any;
+    minutes = Math.floor(seconds / 60);
+    minutes = minutes >= 10 ? minutes : "0" + minutes;
+    seconds = Math.floor(seconds % 60);
+    seconds = seconds >= 10 ? seconds : "0" + seconds;
+    return minutes + ":" + seconds;
+  };
+
+  getPosition = el => {
+    return el.getBoundingClientRect();
+  };
+
+  handleControlClick = ({target}) => {
+    const {isPlaying} = this.state;
+
+    if (isPlaying) {
+      this.player.pause();
+    } else {
+      this.player.play();
+    }
+
+    this.setState({
+      [target.name]: !isPlaying
+    });
+  };
+
+  handleTimelineClick = e => {
+    // console.info(e);
+    this.movePlayhead(e);
+  };
+
+  handleTimeUpdate = () => {
+    const timelineWidth = 300;
+    const playPercent = timelineWidth * (this.player.currentTime / this.duration);
+
+    this.setState({
+      playheadPosition: playPercent,
+      currentDuration: this.formatTime(this.player.currentTime),
+    });
+  }
+
+  handleCanPlayThrough = () => {
+    this.duration = this.player.duration;
+    this.setState({totalDuration: this.formatTime(this.player.duration)})
+  }
+
+  mouseDown = () => {
+    this.mouseOnPlayhead = true;
+    window.addEventListener("mousemove", this.movePlayhead, true);
+  };
+
+  mouseUp = event => {
+    if (this.mouseOnPlayhead) {
+      this.movePlayhead(event);
+      window.removeEventListener("mousemove", this.movePlayhead, true);
+    }
+    this.mouseOnPlayhead = false;
+  };
+
+  movePlayhead = event => {
+    const {left, width: timelineWidth} = this.getPosition(this.timeline);
+    // const { duration } = this.state;
+    var newMargLeft = event.clientX - left;
+    let playheadPosition = newMargLeft;
+
+    if (newMargLeft < 0) {
+      playheadPosition = 0;
+    }
+    if (newMargLeft > timelineWidth) {
+      playheadPosition = timelineWidth;
+    }
+
+    this.setState({
+      playheadPosition,
+      activeTimelineWidth: playheadPosition,
+      currentDuration: this.formatTime(
+          this.duration * (playheadPosition / timelineWidth)
+      )
+    });
+  };
+
+  render() {
+    const {
+      isPlaying,
+      playheadPosition,
+      activeTimelineWidth,
+      currentDuration,
+      totalDuration
+    } = this.state;
+
+    return (
+        <div className="ap">
+          <div className="ap__item">
+            <div className="ap__item__details">
+              <h4 className="ap__item__details__name">Loktak Ema</h4>
+              <h4 className="ap__item__details__artist">Ranbir</h4>
+            </div>
+          </div>
+          <div className="ap__container">
+            <div className="ap__container__wrapper">
+              <div className="ap__container__controls">
+                <button className="ap__icon ap__control ap__right-spacing">
+                  <i className="icon-shuffle"/>
+                </button>
+                <button className="ap__icon ap__control ap__right-spacing">
+                  <i className="icon-control-start"/>
+                </button>
+                <button
+                    name={controlNames.IS_PLAYING}
+                    className={classnames(
+                        "ap__icon ap__circle ap__control",
+                        {
+                          "ap__circle--play": isPlaying,
+                          "ap__circle--pause": !isPlaying
+                        }
+                    )}
+                    onClick={this.handleControlClick}
+                />
+                <button className="ap__icon ap__control ap__left-spacing">
+                  <i className="icon-control-end"/>
+                </button>
+                <button className="ap__icon ap__control ap__left-spacing">
+                  <i className="icon-loop"/>
+                </button>
+              </div>
+              <div className="ap__timeline-container">
+                <div
+                    className="ap__playback-time ap__playback-time__left ap__control">
+                  {currentDuration}
+                </div>
+                <div className="ap__timeline" ref={this.setTimelineRef}>
+                  <div
+                      className="ap__timeline__current"
+                      style={{width: activeTimelineWidth}}
+                  />
+                  <div
+                      className="ap__playhead"
+                      ref={this.setPlayheadRef}
+                      style={{left: playheadPosition}}
+                  />
+                </div>
+                <div
+                    className="ap__playback-time ap__playback-time__right ap__control">
+                  {totalDuration}
+                </div>
+              </div>
+              <audio ref={this.setPlayerRef} src={this.props.src}/>
+            </div>
+          </div>
+          <div className="ap__volume">
+            <div>
+              <div className="ap__volume__icon">
+                <i className="icon-volume-2"/>
+              </div>
+              <div className="ap__volume__controls">
+                <div className="ap__volume__controls__timeline"/>
+              </div>
+            </div>
+          </div>
+        </div>
+    );
+  }
+
+  setTimelineRef = _ref => {
+    this.timeline = _ref;
+    this.timelineWidth = this.timeline.style.width;
+    console.info(this.timeline.getBoundingClientRect());
+    this.timeline.addEventListener("click", this.handleTimelineClick, false);
+  };
+
+  setPlayheadRef = _ref => {
+    this.playhead = _ref;
+    this.playhead.addEventListener("mousedown", this.mouseDown, false);
+  };
+
+  setPlayerRef = _ref => {
+    this.player = _ref;
+    this.player.volume = 0.2;
+    this.player.addEventListener('canplaythrough', this.handleCanPlayThrough, false);
+    this.player.addEventListener('timeupdate', this.handleTimeUpdate, false);
+    this.player.addEventListener('ended', this.handleEnded, false);
+  };
+}
+
+export default AudioPlayer;
