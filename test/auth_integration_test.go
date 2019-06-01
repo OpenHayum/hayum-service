955bddf843af275f19ddcd6888cd4c066c6ffce6
dragfire <asem.devajit@gmail.com>
2019-05-31 22:53:23 -0400

    Refactor login user

--- test/auth_integration_test.go
+++ test/auth_integration_test.go
@@ -3,24 +3,36 @@ package test
 import (
 	"bytes"
 	"encoding/json"
-	"hayum/core_apis/logger"
+	"github.com/stretchr/testify/assert"
 	"hayum/core_apis/models"
 	"hayum/core_apis/routes"
 	"io/ioutil"
 	"net/http"
 	"strconv"
-
-	"github.com/stretchr/testify/assert"
 )
 
-func (s *hayumSuite) loginUser(user *models.User) *http.Response {
+func (s *hayumSuite) loginUser(user *models.User) *route.LoginResponseBody {
 	s.createUser(user)
 	reqBody, err := json.Marshal(&route.LoginRequestBody{Identifier: user.Email, Password: user.Password})
 	s.checkError(err)
 
 	resp, err := s.ts.Client().Post(s.URL("login"), "application/json", bytes.NewReader(reqBody))
 	s.checkError(err)
-	return resp
+
+	if resp.StatusCode != http.StatusOK {
+		s.T().Fatalf("Failed to login %s", resp.Status)
+	}
+
+	defer resp.Body.Close()
+
+	respBody, err := ioutil.ReadAll(resp.Body)
+	s.checkError(err)
+
+	var loginResp route.LoginResponseBody
+	err = json.Unmarshal(respBody, &loginResp)
+	s.checkError(err)
+
+	return &loginResp
 }
 
 // ************************************* Auth(Register/Login) ****************************************
@@ -42,21 +54,7 @@ func (s *hayumSuite) TestRegister() {
 
 func (s *hayumSuite) TestLogin() {
 	user := getUser()
-	resp := s.loginUser(user)
-
-	if resp.StatusCode != http.StatusOK {
-		s.T().Fatalf("Failed to login %s", resp.Status)
-	}
-
-	defer resp.Body.Close()
-
-	respBody, err := ioutil.ReadAll(resp.Body)
-	s.checkError(err)
-
-	var loginResp route.LoginResponseBody
-	err = json.Unmarshal(respBody, &loginResp)
-	s.checkError(err)
-	logger.Log.Info(loginResp)
+	loginResp := s.loginUser(user)
 	assert.True(s.T(), loginResp.User.Email == user.Email, "Emails should be equal")
 
 	truncate(s.Conn)
@@ -64,20 +62,13 @@ func (s *hayumSuite) TestLogin() {
 
 func (s *hayumSuite) TestLogout() {
 	user := getUser()
-	resp := s.loginUser(user)
-
-	respBody, err := ioutil.ReadAll(resp.Body)
-	s.checkError(err)
-
-	var loginResp route.LoginResponseBody
-	err = json.Unmarshal(respBody, &loginResp)
-	s.checkError(err)
+	loginResp := s.loginUser(user)
 
 	req, _ := http.NewRequest("POST", s.URL("logout"), nil)
 	req.Header.Add("user-id", strconv.Itoa(loginResp.User.Id))
 	req.Header.Add("session-id", loginResp.Session.SessionID)
 
-	resp, err = s.ts.Client().Do(req)
+	resp, err := s.ts.Client().Do(req)
 	s.checkError(err)
 
 	if resp.StatusCode != http.StatusOK {
