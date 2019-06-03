ab7dc6cf62bc0f6be1d7250d47e4cbd2e5216471
dragfire <asem.devajit@gmail.com>
2019-06-03 13:54:26 -0400

    Add Protected middleware

---
+++ middleware/middleware.go
@@ -0,0 +1,26 @@
+package middleware
+
+import (
+	"github.com/julienschmidt/httprouter"
+	"github.com/pkg/errors"
+	"hayum/core_apis/config"
+	"hayum/core_apis/db"
+	hyErrors "hayum/core_apis/errors"
+	"hayum/core_apis/logger"
+	"net/http"
+)
+
+func Protected(next httprouter.Handle) httprouter.Handle {
+	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+		session, err := db.Store.Get(r, config.SessionName)
+		hyErrors.CheckAndSendResponseBadRequest(err, w)
+		userVal := session.Values["user"]
+		logger.Log.Info("Middleware:", session, userVal, r.Header.Get("Cookie"), session.IsNew)
+		if userVal == nil {
+			http.Error(w, errors.New("user not logged in").Error(), http.StatusUnauthorized)
+			return
+		}
+
+		next(w, r, ps)
+	}
+}
